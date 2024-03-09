26177ba927d35880c3528a0fb082d19c54856c02
Dámaso Armas <127236212+dasktorm@users.noreply.github.com>
2024-03-09 20:46:12 +0000

    ver. 1.0.0: Impact Display and Refill Input generated

---
+++ agua-kmzero-interface/src/components/ImpactDisplay.vue
@@ -0,0 +1,76 @@
+<template>
+    <div class="impact-display">
+      <p v-if="impactCalculated">
+        <span class="impact-text">Impact:</span>
+        <span class="impact-value">
+          {{ bottlesSaved }} bottles <br>
+          ({{ plasticSaved }} kg of plastic saved)
+        </span>
+      </p>
+      <p v-if="impactCalculated">
+        <span class="impact-text">Did you know?</span>
+        <span class="impact-value">
+          This is the impact of your refill<br>
+          {{ carbonSaved }} kg<br>
+          ({{ co2Emissions }} of CO2 emissions)
+        </span>
+      </p>
+      <p v-if="impactCalculated">
+        <span class="impact-text">(Plastic bottles)</span>
+        <span class="impact-value">
+          Share your Impact!
+        </span>
+      </p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'ImpactDisplay',
+    props: {
+      bottlesSaved: {
+        type: Number,
+        required: true,
+      },
+      plasticSaved: {
+        type: String,
+        required: true,
+      },
+      carbonSaved: {
+        type: String,
+        required: true,
+      },
+      co2Emissions: {
+        type: String,
+        required: true,
+      },
+      impactCalculated: {
+        type: Boolean,
+        required: true,
+      },
+    },
+  };
+  </script>
+  
+  <style>
+  .impact-display {
+    margin-top: 1rem;
+    border: 1px solid #ccc;
+    padding: 0.5rem;
+    background-color: #f8f8f8;
+  }
+  
+  .impact-text {
+    font-weight: bold;
+    display: block;
+    text-align: left;
+    width: 50%;
+  }
+  
+  .impact-value {
+    font-weight: normal;
+    display: block;
+    text-align: right;
+    width: 50%;
+  }
+  </style>
---
+++ agua-kmzero-interface/src/components/RefillInput.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="refill-input">
+      <p>How many reeeefills? {{ reeeefills }}</p>
+      <button @click="addRefill">Add Refill</button>
+      <button @click="calculateImpact" class="calculate-impact-button">Calculate Impact</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'RefillInput',
+    props: {
+      reeeefills: {
+        type: Number,
+        required: true,
+      },
+      onCalculateImpact: {
+        type: Function,
+        required: true,
+      },
+    },
+    methods: {
+      addRefill() {
+        this.$emit('add-refill');
+      },
+      calculateImpact() {
+        this.onCalculateImpact();
+      },
+    },
+  };
+  </script>
+  
+  <style>
+  .refill-input {
+    font-family: Arial, sans-serif;
+    text-align: center;
+    padding: 2rem;
+  }
+  
+  .calculate-impact-button {
+    margin-top: 1rem;
+  }
+  </style>
